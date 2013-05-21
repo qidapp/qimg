6b5fb88127eaceb156e6efddde69e4b834140e1d
Fangfang Zhao <zhaofang123@gmail.com>
2013-05-22 01:31:11 +0800

    zk 0.1

---
+++ zk/promo_discount_view-0.1.js
@@ -0,0 +1,125 @@
+/**
+ * $Id: promo_discount_view.js 3542 2013-05-20 16:57:13Z zhaoff@qidapp.com $
+ */
+var type = "0";
+var seller_cids = "0";
+var promo_limit_count = 0;
+var promo_fullsend_count = 0;
+var promo_freepost_count = 0;
+var catIds = {};
+var render_item = function(){
+	var idx = 0;
+	$(item_data).each(function(ind, item){
+		if ((type != "0" && type != "5" && type.indexOf(item[7]) < 0) || (seller_cids != "0" && seller_cids.indexOf(item[8]) < 0) || (type == "5" && item[6].indexOf('免邮') < 0)) {
+			return true;
+		}
+		if (~~item[7] != 4 && ~~item[7] != 5) {
+			promo_limit_count++;
+		} else {
+			promo_fullsend_count++;
+			if (item[6].indexOf('免邮') >= 0) {
+				promo_freepost_count++;
+			}
+		}
+		if (!!!catIds[item[8]]) { catIds[item[8]] = 1; }
+		else { catIds[item[8]] = catIds[item[8]] + 1;}
+		setTimeout(function(){
+			var _div = $('<div class="item"></div>').appendTo($('#main .span3:eq(' + idx % 4 + ')'));
+			$('<a class="img" href="http://item.taobao.com/item.htm?id='+item[0]+'&ext=zk" target="_blank"><img src=" ' + item[4] + '" /></a>').appendTo(_div);
+			$('<a class="title" href="http://item.taobao.com/item.htm?id='+item[0]+'&ext=zk" target="_blank">'+item[1]+'</a>').appendTo(_div);
+			if (~~item[7] != 4 && ~~item[7] != 5) {
+				var _span = "";
+				if (~~item[7] == 7) {
+					_span = '<div class="flag"><span class="label">会员折扣</span></div>';
+				}
+				$('<div class="price"><span class="pull-left">¥ <b>' + parseFloat(item[5]).toFixed(2) + '</b> <span class="discount">'+item[6]+'折</span></span><span class="pull-right">原价 <del>'+parseFloat(item[2]).toFixed(2)+'</del></span><div class="clearfix"></div>'+_span+'</div>').appendTo(_div);
+			} else {
+				var _span = "";
+				var promoC = item[6].split(';');
+				for (var s in promoC) {
+					_span += "<span class='label'>" + promoC[s] + "</span>";
+				}
+				$('<div class="price"><span>¥ <b>' + parseFloat(item[2]).toFixed(2) + '</b></span></div><div class="flag">'+_span+'</div>').appendTo(_div);
+			}
+			idx++;
+			if (!$('#tip').hasClass('hide')) {
+				$('#tip').addClass('hide');
+			}
+		}, ind * 25);
+	});
+	if (idx == 0) {
+		$('#tip').removeClass('hide');
+	}
+};
+
+var promo_type_active, seller_cids_active;
+$(document).ready(function(){
+	$('#promo_type a').click(function(){
+		type = $(this).attr('_value');
+		$('#main .span3').empty();
+		if (!!promo_type_active) {
+			$(promo_type_active).removeClass('active');
+		}
+		$(this).addClass('active');
+		promo_type_active = this;
+		render_item();
+	});
+	
+	$('#seller_cids a').click(function(){
+		seller_cids = $(this).attr('_value');
+		$('#main .span3').empty();
+		if (!!seller_cids_active) {
+			$(seller_cids_active).removeClass('active');
+		}
+		$(this).addClass('active');
+		seller_cids_active = this;
+		render_item();
+	});
+	
+	$('#search_tb').click(function(){
+		$('#search').attr('action', 'http://z.alimama.com/tksEss.php');
+	});
+	
+	$('#search_self').click(function(){
+		$('#search').attr('action', 'http://itools.qdss.org/app/TaobaoSearchEncode');
+	});
+	
+	$(window).scroll(function(){
+	    if($(document).scrollTop() >= 78) {
+	    	if (!$('#promoType').hasClass('float')){
+	    		$('#item-area').css('margin-top', $('#promoType').height() + 42);
+	    		$('#promoType').addClass('float');
+	    	}
+	    } else {
+	    	$('#item-area').css('margin-top', 0);
+	    	$('#promoType').removeClass('float');
+	    }
+	});
+	
+	$('#share').attr('href', 'http://www.jiathis.com/send/?webid=tsina&url='+encodeURIComponent('http://itools.qidapp.cn/Link.html?id=itools')+'&title='+encodeURIComponent("好多折扣商品，赶快来看看吧！ #掌柜工具箱# "+location.href)+'&uid=1714783&appkey=3822648575');
+});
+
+var get_cids = function() {
+	var aHtml = "";
+	$(shop_cats).each(function(ind, item){
+		var _aValue = item.cid;
+		var nodeHasItem = false;
+		var count = 0;
+		if (!!catIds[',' + item.cid + ',']) {
+			nodeHasItem = true;
+			count = catIds[',' + item.cid + ','];
+		}
+		
+		$(item.child).each(function(ind, item){
+			_aValue += "," + item.cid;
+			if (!!catIds[',' + item.cid + ',']) {
+				nodeHasItem = true;
+				count += catIds[',' + item.cid + ','];
+			}
+		});
+		if (!!nodeHasItem) {
+			aHtml += '<a href="#'+item.name+'" _value=",'+_aValue+',">' + item.name + ' <span>(' + count + ')</span></a>';
+		}
+	});
+	$(aHtml).appendTo($('#seller_cids'));
+};
