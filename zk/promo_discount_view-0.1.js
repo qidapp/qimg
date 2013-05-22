132fe7e654b4e657819d4b9a702d61e9f469c9d9
Fangfang Zhao <zhaofang123@gmail.com>
2013-05-22 12:26:04 +0800

    zk j2

--- zk/promo_discount_view-0.1.js
+++ zk/promo_discount_view-0.1.js
@@ -120,7 +120,8 @@ var get_cids = function() {
 			}
 		});
 		if (!!nodeHasItem) {
-			aHtml += '<a href="#'+item.name+'" _value=",'+_aValue+',">' + item.name + ' <span class="hide">(' + count + ')</span></a>';
+			//aHtml += '<a href="#'+item.name+'" _value=",'+_aValue+',">' + item.name + ' <span>(' + count + ')</span></a>';
+			aHtml += '<a href="#'+item.name+'" _value=",'+_aValue+',">' + item.name + '</a>';
 		}
 	});
 	$(aHtml).appendTo($('#seller_cids'));
